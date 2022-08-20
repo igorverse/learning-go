340453763eb6275e3503a1e759ce68469c705504
Igor Silva <osilvaigor@gmail.com>
2022-08-20 16:01:39 -0300

    feat: add methods example

---
+++ go-by-example/methods.go
@@ -0,0 +1,32 @@
+// Go supports methods defined on struct types
+
+package main
+
+import "fmt"
+
+type rect struct {
+	width, height int
+}
+
+// This area method has a receiver type of *rect
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+// Here's an example of a method defined with value receiver
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
+}
+
+func main() {
+	r := rect{width: 10, height: 5}
+
+	fmt.Println("area: ", r.area())
+	fmt.Println("perim: ", r.perim())
+
+	rp := &r
+
+	// Go automatically handles conversion between values and pointers for method calls
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim: ", rp.perim())
+}
